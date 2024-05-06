847b0d8d695c22d81769e06a405aae7cf2005d66
Romulo Menezes de Santana <romulomsantana@hotmail.com>
2024-05-06 10:10:07 -0300

    fix sidebar bugs

--- app/app4966/views.py
+++ app/app4966/views.py
@@ -5,7 +5,7 @@ from django.http import HttpRequest
 
 def index(request):
     print("########################################")
-    return render(request, 'base/sidebar.html')
+    return render(request, 'base/footer.html')
 
 def chat(request):
     print("########################################")
