1cb7f3f10e79660ae55354bf12fb84850440afb9
Júlio Rafael Souza Peixeto <4063090@banese.com.br>
2024-05-17 11:27:07 -0300

    Update backend SCI

--- app/app4966/views.py
+++ app/app4966/views.py
@@ -2,27 +2,43 @@ from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
-from .utils import enviar_prompt_chatgpt
-
+from .utils import (
+    enviaPrompt,
+    enviaPromptPreview
+)
 
 def index(request):
     return render(request, "app4966/home.html")
 
 
 def sci(request):
-    print(request.POST)
-    prompt_value = request.POST.get('prompt', '')
-    context = {
-        'prompt_value': prompt_value,
-    }
-    return render(request, "app4966/sci.html", context)
+    if request.method == "POST":
+        if request.POST.get("entrada", "") == "":
+            prompt_value = request.POST.get("prompt", "")
+            response = enviaPromptPreview(prompt_value)
+            context = {
+                "prompt_value": prompt_value,
+                "response": response
+            }
+            return render(request, "app4966/sci.html", context)
+        
+        else:
+            prompt_value = request.POST.get("prompt", "")
+            response = enviaPromptPreview(prompt_value)
+            context = {
+                "prompt_value": prompt_value,
+                "response": response
+            }
+            return render(request, "app4966/sci.html", context)
+    
+    return render(request, "app4966/sci.html")
 
 
 def teste_api(request):
     if request.method == "POST":
         try:
             prompt = request.POST["prompt"]
-            response = enviar_prompt_chatgpt(prompt)
+            response = enviaPrompt(prompt)
             return render(request, "app4966/example.html", {"response": response})
 
         except Exception as e:
