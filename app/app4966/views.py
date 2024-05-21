5d944921e57066b0b341eb623b7b670fb9fb3883
Julio Rafael <96303574+JulioRaf4@users.noreply.github.com>
2024-05-21 18:36:40 -0300

    Segundo Update Tela SCI

--- app/app4966/views.py
+++ app/app4966/views.py
@@ -1,3 +1,4 @@
+import json
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
@@ -41,3 +42,22 @@ def teste_api(request):
             print(e)
 
     return render(request, "app4966/example.html")
+
+
+def download_json(request: HttpRequest) -> HttpResponse:
+    if request.method == "POST":
+        prompt_value = request.POST.get("prompt", "")
+        response_data = enviaPromptPreview(prompt_value)
+        
+        try:
+            response_json = json.loads(response_data)
+        except json.JSONDecodeError:
+            return HttpResponse("Erro ao decodificar o JSON.", status=400)
+
+        response_json_str = json.dumps(response_json, indent=4)
+
+        response = HttpResponse(response_json_str, content_type='application/json')
+        response['Content-Disposition'] = 'attachment; filename="response.json"'
+        return response
+
+    return HttpResponse("Método não permitido.", status=405)
