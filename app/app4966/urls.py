b13c6f50e7b4dd7cfac9d48eb89a12912c357c9d
Julio Rafael <96303574+JulioRaf4@users.noreply.github.com>
2024-05-22 16:29:16 -0300

    Update fix

--- app/app4966/urls.py
+++ app/app4966/urls.py
@@ -6,6 +6,4 @@ urlpatterns = [
     path('', view=index, name='index'),
     path('sci', view=sci, name='sci'),
     path('teste_api', view=teste_api, name='teste_api'),
-    path('download_json/', download_json, name='download_json'),
-
 ]
