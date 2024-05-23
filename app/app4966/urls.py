31a6df6049db15f4bc3afb637d3e5a62a981ae65
Julio Rafael <96303574+JulioRaf4@users.noreply.github.com>
2024-05-23 15:46:21 -0300

    Update Rotas e templates

--- app/app4966/urls.py
+++ app/app4966/urls.py
@@ -4,6 +4,10 @@ from .views import *
 
 urlpatterns = [
     path('', view=index, name='index'),
-    path('sci', view=sci, name='sci'),
+    path('sci_provisionamento', view=sci_provisionamento, name='sci_provisionamento'),
+    path('sci_relatorio', view=sci_relatorio, name='sci_relatorio'),
+    path('sci_historico', view=sci_historico, name='sci_historico'),
+    path('chat_historico', view=chat_historico, name='chat_historico'),
+
     path('teste_api', view=teste_api, name='teste_api'),
 ]
--- app/app4966/views.py
+++ app/app4966/views.py
@@ -13,13 +13,13 @@ def index(request):
     return render(request, "app4966/home.html")
 
 
-def sci(request):
+def sci_provisionamento(request):
     """Função para lidar com requisições HTTP para a página SCI.
 
     Essa função trata requisições POST para processar dados do formulário
     e gerar respostas apropriadas usando funções auxiliares. Ela também
     renderiza a página 'sci.html' com o contexto atualizado.
-    """    
+    """
     context = {}
 
     if request.method == "POST":
@@ -31,11 +31,25 @@ def sci(request):
             context["response"] = enviaPromptPreview(prompt_value)
         else:
             saida_value = request.POST.get("saida", "")
-            context["response"] = enviaPromptSCI(entrada_value, entrada_value, saida_value)
+            context["response"] = enviaPromptSCI(
+                entrada_value, entrada_value, saida_value
+            )
 
         context["prompt_value"] = prompt_value
 
     return render(request, "app4966/sci.html", context)
+
+
+def sci_relatorio(request): 
+    return render(request, "app4966/sci_relatorio.html")
+
+
+def sci_historico(request): 
+    return render(request, "app4966/sci_historico.html")
+
+
+def chat_historico(request): 
+    return render(request, "app4966/chat_historico.html")
 
 
 def teste_api(request):
