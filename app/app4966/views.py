c5db71c5ff2dcba0280cd46a2452c7e09f0ee34f
Romulo Menezes de Santana <romulomsantana@hotmail.com>
2024-05-23 09:17:11 -0300

    cria funcao para armazenar no mongodb

--- app/app4966/views.py
+++ app/app4966/views.py
@@ -4,7 +4,6 @@ from django.views.decorators.csrf import csrf_exempt
 from .models import Chat
 import datetime
 from pymongo import MongoClient
-from pymongo.server_api import ServerApi
 from django.conf import settings
 
 from .utils import (
@@ -39,41 +38,13 @@ def sci(request):
     
     return render(request, "app4966/sci.html")
 
-# def teste_api(request):
-#     if request.method == "POST":
-#         try:
-#             prompt = request.POST["prompt"]
-#             # response = enviaPrompt(prompt)
-#             response = "xulio vacilao"
-            
-#             # Armazenar no banco de dados
-#             chat_entry = Chat(prompt=prompt, response=response)
-#             chat_entry.save()
-            
-#             return render(request, "app4966/example.html", {"response": response})
-
-#         except Exception as e:
-#             print(e)
-
-#     return render(request, "app4966/example.html")
-
 def teste_api(request):
     if request.method == "POST":
         try:
             prompt = request.POST["prompt"]
             response = "xulio vacilao"
             
-            # Conecte ao MongoDB
-            client = MongoClient(settings.MONGODB_URI)
-            db = client[settings.MONGODB_DATABASE]
-            collection = db[settings.MONGODB_COLLECTION]
-
-            # Insira os dados na coleção
-            collection.insert_one({
-                "prompt": prompt,
-                "response": response,
-                "timestamp": datetime.datetime.now()
-            })
+            armazenaReqResponse(prompt=prompt, response=response)
 
             return render(request, "app4966/example.html", {"response": response})
 
